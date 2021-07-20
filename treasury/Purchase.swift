10bbbb699bbbf34b4533e6706dcbc610fefa5153
Lev Baryshev <l.baryshev@squaregps.com>
2021-07-20 12:34:46 +0500

    master - introducing Purchase

--- treasury/Purchase.swift
+++ treasury/Purchase.swift
@@ -7,4 +7,16 @@
 
 import Foundation
 
-final class Purchase {}
+final class Purchase {
+    
+    let price: Decimal
+    let category: Category
+    let name: String?
+    
+    init(_ price: Decimal, _ category: Category, _ name: String? = nil) {
+        self.price = price
+        self.category = category
+        self.name = name
+    }
+    
+}
