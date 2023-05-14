92df74e6104a1ba46b9e977762b6401eb215c265
sugarbaron <l.baryshev@squaregps.com>
2023-05-14 18:19:46 +0400

    [newborn] SingleRecord refactoring

--- treasury/SdkToolkit/System/Storages/SingleRecord/SingleRecord.swift
+++ treasury/SdkToolkit/System/Storages/SingleRecord/SingleRecord.swift
@@ -2,34 +2,118 @@
 //  SingleRecord.swift
 //  treasury
 //
-//  Created by sugarbaron on 09.04.2023.
+//  Created by sugarbaron on 23.04.2023.
 //
 
-public protocol SingleRecord {
-    
-    associatedtype CoreDataRecordFields : CoreDataRecord where CoreDataRecordFields.DataClass : SingleRecordFields
-    
-    var engine: StorageEngine<CoreDataRecordFields> { get }
-    
-    typealias RecordFields = CoreDataRecordFields.DataClass
-    
-}
+import Foundation
+import Combine
 
-public extension SingleRecord {
+// MARK: constructor
+@dynamicMemberLookup
+public final class SingleRecord<CoreDataRecordFields:CoreDataRecord>
+where CoreDataRecordFields.DataClass : SingleRecordFields {
     
-    subscript<T>(dynamicMember field: KeyPath<RecordFields, T>) -> T { engine.load(field) }
+    private let storage: CoreDataStorage.Engine
+    private var cache: RecordFields
+    private let cacheAccess: NSRecursiveLock
+    private let downstream: CurrentValueSubject<RecordFields, Never>
     
-    subscript<T>(dynamicMember field: WritableKeyPath<RecordFields, T>) -> T {
-        get { engine.load(field) }
-        set { engine.save(field, newValue) }
+    public init?(xcdatamodel fileName: String) {
+        guard let storage: CoreDataStorage.Engine = .init(xcdatamodel: fileName)
+        else { Log(error: "[SingleRecord][\(type(of: RecordFields.self))] unable to construct"); return nil }
+        
+        self.storage = storage
+        self.cache = storage.load(CoreDataRecordFields.self)
+        self.cacheAccess = NSRecursiveLock()
+        self.downstream = CurrentValueSubject<RecordFields, Never>(cache)
+        downstream.send(cache)
     }
-    
-    
     
 }
 
 public protocol SingleRecordFields {
     
     static var empty: Self { get }
+    
+}
+
+// MARK: interface
+public extension SingleRecord {
+    
+    subscript<T>(dynamicMember field: KeyPath<RecordFields, T>) -> T { readCache(field) }
+    
+    subscript<T>(dynamicMember field: WritableKeyPath<RecordFields, T>) -> T {
+        get { readCache(field) }
+        set { update(field, with: newValue) }
+    }
+    
+    func keepInformed<T:Equatable>(about field: KeyPath<RecordFields, T>) -> AnyPublisher<T, Never> {
+        downstream
+            .compactMap { $0[keyPath: field] }
+            .removeDuplicates { new, old in new == old }
+            .anyPublisher
+    }
+    
+    func keepInformed<T>(about field: KeyPath<RecordFields, T>, skip: @escaping (T, T) -> Bool)
+    -> AnyPublisher<T, Never> {
+        downstream.compactMap { $0[keyPath: field] }
+            .removeDuplicates { skip($0, $1) }
+            .anyPublisher
+    }
+    
+    func erase() {
+        cacheAccess.lock()
+        cache = .empty
+        cacheAccess.unlock()
+        storage.write { $0.delete(all: CoreDataRecordFields.self) }
+    }
+    
+    typealias RecordFields = CoreDataRecordFields.DataClass
+    
+}
+
+// MARK: tools
+private extension SingleRecord {
+    
+    func load() -> RecordFields { storage.read { $0.loadAll(CoreDataRecordFields.self).first } ?? .empty }
+    
+    func readCache<T>(_ fieldKey: KeyPath<RecordFields, T>) -> T {
+        cacheAccess.lock()
+        let field: T = cache[keyPath: fieldKey]
+        cacheAccess.unlock()
+        return field
+    }
+    
+    func update<T>(_ field: WritableKeyPath<RecordFields, T>, with new: T) {
+        let updated: RecordFields = updateCache(field, with: new)
+        downstream.send(updated)
+        storage.update(single: updated, as: CoreDataRecordFields.self)
+    }
+    
+    func updateCache<T>(_ field: WritableKeyPath<RecordFields, T>, with new: T) -> RecordFields {
+        cacheAccess.lock()
+        cache[keyPath: field] = new
+        let updated: RecordFields = cache
+        cacheAccess.unlock()
+        return updated
+    }
+    
+}
+
+private extension CoreDataStorage.Engine {
+    
+    func load<R:CoreDataRecord>(_ recordType: R.Type) -> R.DataClass where R.DataClass : SingleRecordFields {
+        read { $0.loadAll(R.self).first } ?? .empty
+    }
+    
+    func update<R:CoreDataRecord>(single original: R.DataClass, as record: R.Type) {
+        write { storage in
+            if let record: R = storage.loadAll().first {
+                record.fill(with: original)
+            } else {
+                storage.create(new: original, as: R.self)
+            }
+        } catch: { Log(error: "[SingleRecordStorage] updateSingle() error") }
+    }
     
 }
